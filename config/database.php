8b7dfd04e0440ff2b6098523a801efd87942858b
otho <contact@otho.bike>
2024-03-17 04:26:03 +0100

    Use in memory database for phpunit
    Seems better to not pollute our database.
    It also save us the hassle of using a separate testing environment

--- config/database.php
+++ config/database.php
@@ -35,6 +35,12 @@ return [
 
     'connections' => [
 
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
